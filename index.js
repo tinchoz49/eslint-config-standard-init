10104928609b6bf618555b35e96aeec9e0bac7d5
Martín Acosta <tinchoz49@gmail.com>
2022-04-28 15:22:47 -0300

    added standard-jsx

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 module.exports = {
   env: {
-    browser: true,
     es2021: true
   },
   extends: [
-    'plugin:react/recommended',
-    'standard'
+    'standard',
+    'standard-jsx'
   ],
   parserOptions: {
     ecmaFeatures: {
@@ -14,9 +13,6 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: [
-    'react'
-  ],
   rules: {
     "react/prop-types": 0
   }
